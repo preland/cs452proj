7def158f41bfd4fc3840da9faf248b3c77e276d7
preland <prelandofficialmusic@gmail.com>
2025-06-18 11:48:04 -0600

    por

--- scraper/src/db.ts
+++ scraper/src/db.ts
@@ -2,6 +2,7 @@ import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
     host: process.env.DB_HOST,         // e.g., AWS RDS endpoint
+    port: Number(process.env.DB_PORT), 
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
